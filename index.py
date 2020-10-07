407e58a9aa99abf8b3f62c5452ba27362ac24ba4
deyam <deyanemo@gmail.com>
2020-10-07 22:28:36 +0200

    web server

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 import argparse
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+@app.route("/webhock", methods=["POST"])
+def listen_to_hock():
+    print(request.json);
+    return Response(status=200)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
